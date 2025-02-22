d38f344dc3f5e50e7b4f7f8db2b116001bac870e
Trey Davis <89280903+TreyDavis3@users.noreply.github.com>
2025-02-22 13:33:19 -0700

    First commit. Open txt file

---
+++ main.py
@@ -0,0 +1,6 @@
+try:
+    with open("GP_winners.txt", "r") as file:  # Open the file in read mode ('r')
+        file_content = file.read()  # Read the entire file content
+        print(file_content)
+except FileNotFoundError:
+    print("Error: File 'my_data.txt' not found.")
